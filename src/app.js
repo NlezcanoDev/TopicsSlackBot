6eb9009d3b6cbf52cb98623e81a9ab24a30d5f84
Nahuel Lezcano <nlezcano.dev@gmail.com>
2023-08-03 13:00:05 -0300

    Update metrics service

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import morgan from "morgan";
+// import morgan from "morgan";
 import { errorHandler } from "./middlewares/errorHandler";
 
 import topicsRoutes from "./routes/topic.route";
@@ -9,7 +9,6 @@ import cronjobRoutes from "./routes/cron.route";
 import { openAiService } from "./services/OpenAi";
 import { slackService } from "./services/Slack";
 import { cronJobService } from "./services/CronJobs";
-import { createLog } from "./utils/db";
 
 const app = express();
 
@@ -17,7 +16,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 // middlewares
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -29,11 +28,7 @@ cronJobService.init();
 app.use("/api/topics", topicsRoutes);
 app.use("/api/slack", slackRoutes);
 app.use("/api/cronjob", cronjobRoutes);
-app.use("/metrics", (res, req) => {
-	createLog(req.getHeader());
 
-	res.status(200);
-});
 app.use((_, res) => res.status(404).send("Not found"));
 
 app.use(errorHandler);
